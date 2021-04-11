dbe4a2753019538f9d882e0e12540e44bd82d051
gorzer <gorzer.rostyslav@gmail.com>
2021-04-11 22:21:37 +0300

    added animation

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,16 @@
 $(function() {
   const $gallery = $('.gallery a').simpleLightbox();
 });
+
+const button = document.querySelector('.btn');
+
+console.log(button);
+
+button.addEventListener('mousedown', (e) => {
+  button.style.boxShadow = 'none';
+  button.style.transform = 'translateY(0)';
+});
+
+button.addEventListener('mouseup', (e) => {
+  button.style = {};
+});
